6e18049506bff072b0eef3245c8c00f7bef6852d
Neal_Eleazar_J_Ramos <nealramos72@gmail.com>
2026-09-07 11:22:31 +0800

    Add Routes

--- order-service/src/routes/OrderRoutes.ts
+++ order-service/src/routes/OrderRoutes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import { ValidateBody } from "../middleware/ValidateBody.js";
+import { CreateOrderDto } from "../features/Order/Commands/CreateOrder/CreateOrderDto.js";
+import { GetByIdDto } from "../features/Order/Queries/GetById/GetByIdDto.js";
+import { OrderController } from "../controllers/OrderController.js";
 
 const OrderRouter = Router()
+const orderController = new OrderController()
 
-OrderRouter.post("/Order", () => {return 0})
-OrderRouter.get("/Order/:OrderId", () => {return 0})
+OrderRouter.post("/Order", ValidateBody(CreateOrderDto), orderController.CreateOrder)
+OrderRouter.get("/Order/:OrderId", ValidateBody(GetByIdDto), orderController.GetByOrderId)
 
 export default OrderRouter
